34044e406d677030d02bd2aa81f93ba73f224b12
Maimito <64570623+Maimito@users.noreply.github.com>
2021-09-10 21:19:43 +0700

    update widget

--- app/src/main/java/com/mbkm/tugas8_gits/MainActivity.java
+++ app/src/main/java/com/mbkm/tugas8_gits/MainActivity.java
@@ -2,18 +2,20 @@ package com.mbkm.tugas8_gits;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import com.dd.processbutton.iml.ActionProcessButton;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputEditText mNamaLengkap;
     private TextInputEditText mEmail;
     private TextInputEditText mPassword;
-    private Button mDeleteAll;
+    private ActionProcessButton mDeleteAll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,26 @@ public class MainActivity extends AppCompatActivity {
         mNamaLengkap = (TextInputEditText) findViewById(R.id.namaLengkap);
         mEmail = (TextInputEditText) findViewById(R.id.email);
         mPassword = (TextInputEditText) findViewById(R.id.password);
-        mDeleteAll = (Button) findViewById(R.id.deleteAll);
+        mDeleteAll = (ActionProcessButton) findViewById(R.id.deleteAll);
+
+        Handler handler = new Handler();
 
         mDeleteAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mDeleteAll.setMode(ActionProcessButton.Mode.PROGRESS);
+                mDeleteAll.setProgress(0);
                 mNamaLengkap.setText("");
                 mEmail.setText("");
                 mPassword.setText("");
+                mDeleteAll.setProgress(100);
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mDeleteAll.setProgress(0);
+                    }
+                }, 2000);
             }
         });
     }
